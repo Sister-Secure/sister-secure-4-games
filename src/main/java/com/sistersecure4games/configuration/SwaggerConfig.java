a138d1965e406d70bcf6950337207f99e762fb1e
Dafne Duda <dafne.leslie@gmail.com>
2024-05-25 16:40:30 -0300

    deploy do projeto. Co-authored-by: Larissa Sanches <larissaesanches@gmail.com> Co-authored-by: Sabrina Furtado <sabrina.s.furtado@gmail.com> Co-authored-by: Asttryd Pacheco <asttryd.s.pacheco@gmail.com>

---
+++ src/main/java/com/sistersecure4games/configuration/SwaggerConfig.java
@@ -0,0 +1,56 @@
+package com.sistersecure4games.configuration;
+
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+
+@Configuration
+public class SwaggerConfig {
+
+@Bean
+OpenAPI springSisterSecureOpenAPI() {
+return new OpenAPI()
+.info(new Info().title("Projeto Sister Secure 4 Games").description("Projeto Sister Secure - 4 Games")
+.version("v0.0.1")
+.license(new License().name("Sister Secure 4 Games").url("https://github.com/Sister-Secure/sister-secure-4-games"))
+.contact(new Contact().name("Sister Secure 4 Games").url("https://github.com/Sister-Secure/sister-secure-4-games")
+.email("sistersecure4@gmail.com")))
+.externalDocs(new ExternalDocumentation().description("Github")
+.url("https://github.com/Sister-Secure/sister-secure-4-games"));
+}
+
+@Bean
+OpenApiCustomizer customerGlobalHeaderOpenApiCustomiser() {
+
+return openApi -> {
+openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+
+ApiResponses apiResponses = operation.getResponses();
+
+apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
+apiResponses.addApiResponse("201", createApiResponse("Objeto Persistido!"));
+apiResponses.addApiResponse("204", createApiResponse("Objeto Excluido!"));
+apiResponses.addApiResponse("400", createApiResponse("Erro na Requisiçao!"));
+apiResponses.addApiResponse("401", createApiResponse("Acesso Nao Autorizado!"));
+apiResponses.addApiResponse("403", createApiResponse("Acesso Proibido!"));
+apiResponses.addApiResponse("404", createApiResponse("Objeto Nao Encontrado!"));
+apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
+
+}));
+
+};
+}
+
+private ApiResponse createApiResponse(String message) {
+
+return new ApiResponse().description(message);
+}
+}
